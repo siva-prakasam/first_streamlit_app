74e55a6bf568968845c260e69ef7465113a6ccf2
siva-prakasam <118800108+siva-prakasam@users.noreply.github.com>
2023-03-09 21:51:07 +0530

    Update streamlit_app.py
    added sf code

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 #streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
