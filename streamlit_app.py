155554ca2cf8b83d001f5324885352c8765fcd1f
siva-prakasam <118800108+siva-prakasam@users.noreply.github.com>
2023-03-09 23:25:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,9 +47,9 @@ except URLError as e:
 #streamlit.stop()
 
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 #my_data_row = my_cur.fetchone()
 #streamlit.text("The Fruit List Containner:")
 #streamlit.text(my_data_row)
