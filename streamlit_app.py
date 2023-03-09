285e77c906ecb7c77847f2bf499c1757dc0b51dd
siva-prakasam <118800108+siva-prakasam@users.noreply.github.com>
2023-03-09 22:28:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 # write your own comment - what does this do?
 #streamlit.dataframe(fruityvice_normalized)
 
+# STOPS services
+streamlit.stop()
+
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
